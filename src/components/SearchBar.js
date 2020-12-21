232d1c944011b5f695c6b4e14e315a43392b75e9
1851788 <1654475161@qq.com>
2020-12-21 20:56:26 +0800

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { AutoComplete, Input } from 'antd';
+import {AutoComplete, Input} from 'antd';
 import BaseComponent from './BaseComponent'
-import { withRouter } from "react-router-dom";
-import { connect } from 'react-redux';
-import { setKeyword } from '../redux/actions/action'
+import {withRouter} from "react-router-dom";
+import {connect} from 'react-redux';
+import {setKeyword} from '../redux/actions/action'
 
 const mapStateToProps = state => ({
     keyword: state.keywordReducer.keyword,
@@ -11,33 +11,39 @@ const mapStateToProps = state => ({
 
 const { Option } = AutoComplete
 
+var key = 0
+
 class SearchBar extends BaseComponent {
 
     constructor(props) {
         super(props);
         this.state = {
-            query: "",
             isEnter: false,
             dataSource: [],
+            name: ""
         }
-    }
+    };
 
     search = () => {
-        const keyword = this.state.query
+        const keyword = this.state.name
         this.props.dispatch(setKeyword(keyword))
         if (!keyword || keyword === "") {
-            this.pushNotification("warning", "关键词不能为空")
+            this.pushNotification("warning", "Keyword shouldn't be empty.")
             return
         }
-        this.props.history.push({ pathname: "/user/", state: { keyword } })
-        this.timeout(1).then(() => this.props.history.push({ pathname: "/user/search", state: { keyword } }))
+        this.props.history.push({pathname: "/user/", state: {keyword}})
+        this.timeout(1).then(() =>
+
+            this.props.history.push({pathname: "/user/search", state: {keyword}}))
+
     }
 
     fetchAutoComplete = (value) => {
         console.log(value)
         var successAction = (result) => {
             const group = result.data
-            this.setState({ dataSource: group });
+            console.log(group)
+            this.setState({dataSource: group});
         }
         this.get('/api/question/searchRecommend?keywords=' + value + '&limits=5', successAction)
     }
@@ -45,7 +51,7 @@ class SearchBar extends BaseComponent {
     autoOnChange = (value) => {
         var val = value.split(',')[0]
         this.setState({
-            query: value
+            name: value
         })
         if (this.timer) {
             clearTimeout(this.timer);
@@ -58,15 +64,19 @@ class SearchBar extends BaseComponent {
     renderSearch = () => {
         let style = {}
         if (this.state.isEnter)
-            style = { width: '100%', opacity: 1, fontFamily: "Georgia" }
+            style = {width: '100%', opacity: 1, fontFamily: "Georgia"}
         else
-            style = { width: '100%', opacity: 0.4, fontFamily: "Georgia" }
+            style = {width: '100%', opacity: 0.4, fontFamily: "Georgia"}
         return (
             <AutoComplete
                 size="large"
                 style={style}
-                value={this.state.query}
-                dataSource={this.state.dataSource.map(item => <Option key={item}>{item}</Option>)}
+                defaultActiveFirstOption={false}
+                dataSource={this.state.dataSource.map(item=>{
+                    key = key + 1;
+                    var uqKey= item+','+key
+                    return <Option key={uqKey}>{item}</Option>
+                })}
                 onChange={this.autoOnChange}
                 placeholder="Search For An Existing QA">
                 <Input.Search
@@ -79,11 +89,12 @@ class SearchBar extends BaseComponent {
 
     render() {
         return (
-            <div onClick={() => this.setState({ isEnter: true })}>
+            <div
+                onClick={() => this.setState({isEnter: true})}>
                 {this.renderSearch()}
             </div>
         );
     }
 }
 
-export default connect(mapStateToProps)(withRouter(SearchBar));
+export default withRouter(connect(mapStateToProps)(SearchBar));
